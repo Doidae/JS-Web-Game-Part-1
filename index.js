f37f86e434a8142b9399a037ee9536173ab60144
Doidae <andyarredondo1224@gmail.com>
2023-11-15 22:09:11 -0500

    removed the comment that was left

--- index.js
+++ index.js
@@ -6,19 +6,6 @@ function newImage (imgName,fileLocation,posit,left,bottom){
     imgName.style.bottom = bottom
     document.body.append(imgName)
 }
-// function newItem (fileLocation,positon,left,bottom){
-//     let sword = document.createElement('img')
-//     sword.src = fileLocation
-//     sword.position = positon
-//     sword.left = left
-//     sword.bottom = bottom
-//     document.body.append(sword)
-
-//     sword.addEventListener('click', function(){
-//         sword.remove()
-//     })
-
-// }
 
 function newItem(loc,position,lef,bot){
         let sword = document.createElement('img')
@@ -28,7 +15,7 @@ function newItem(loc,position,lef,bot){
     sword.style.bottom = bot
     document.body.append(sword)
 
-sword.addEventListener('click', function(){
+sword.addEventListener('dblclick', function(){
     sword.remove()
 })
 
@@ -40,11 +27,16 @@ let tree
 let pillar
 let crate
 let well
+
 let item = newItem('assets/sword.png','fixed','500px','405px')
+let shield = newItem('assets/shield.png','fixed','165px','185px')
+let staff = newItem('assets/staff.png','fixed','600px','100px')
+
 
 newImage(greenCharacter,'assets/green-character.gif','fixed','100px','100px')
-newImage(pineTree,'assets/pine-tree.png','fixed','450px','100px')
+newImage(pineTree,'assets/pine-tree.png','fixed','450px','200px')
 newImage(tree,'assets/tree.png','fixed','200px','300px')
 newImage(pillar,'assets/pillar.png','fixed','350px','100px')
 newImage(crate,'assets/crate.png','fixed','150px','200px')
 newImage(well,'assets/well.png','fixed','500px','425px')
+
